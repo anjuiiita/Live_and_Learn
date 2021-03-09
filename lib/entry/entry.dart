44d9fdbb7536959d577d6be8fcf987154ae7927d
WMXPY <wm@mengw.io>
2021-03-09 16:33:13 -0600

    succeed draft

--- lib/entry/entry.dart
+++ lib/entry/entry.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:live_and_learn/entry/example.dart';
 
-class LiveAndLearnEntry extends StatelessWidget {
+class LiveAndLearnEntry extends StatefulWidget {
+  @override
+  _LiveAndLearnEntryState createState() => _LiveAndLearnEntryState();
+}
+
+class _LiveAndLearnEntryState extends State<LiveAndLearnEntry> {
+  @override
+  void initState() {
+    super.initState();
+
+    final channelName = 'app.sudo.land/live_and_learn/example';
+    final methodChannel = MethodChannel(channelName);
+    methodChannel.setMethodCallHandler((MethodCall call) async {
+      print(call);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/tutorial/tutorial.dart
+++ lib/tutorial/tutorial.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:hardware_buttons/hardware_buttons.dart';
 import 'package:live_and_learn/entity/tutorial.dart';
 
 class TutorialView extends StatefulWidget {
@@ -16,22 +13,14 @@ class TutorialView extends StatefulWidget {
 }
 
 class _TutorialViewState extends State<TutorialView> {
-  StreamSubscription<VolumeButtonEvent> _volumeButtonSubscription;
-
   @override
   void initState() {
     super.initState();
-    _volumeButtonSubscription = volumeButtonEvents.listen(
-      (VolumeButtonEvent event) {
-        print(event);
-      },
-    );
   }
 
   @override
   void dispose() {
     super.dispose();
-    _volumeButtonSubscription?.cancel();
   }
 
   @override
