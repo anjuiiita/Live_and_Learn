c2f393f9319c76da3ed7faf906437c1c0a5863b8
WMXPY <wm@mengw.io>
2021-03-09 17:18:47 -0600

    add void permission requesting

--- lib/entry/entry.dart
+++ lib/entry/entry.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:live_and_learn/entry/example.dart';
 
