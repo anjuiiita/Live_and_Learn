dbb46c6f343740f1ea40c8e14fa166d4f74fe9c3
WMXPY <wm@mengw.io>
2021-03-23 15:44:51 -0500

    add multple init

---
+++ lib/example/speech.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SpeechExampleView extends StatefulWidget {
+  @override
+  _SpeechExampleViewState createState() => _SpeechExampleViewState();
+}
+
+class _SpeechExampleViewState extends State<SpeechExampleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
