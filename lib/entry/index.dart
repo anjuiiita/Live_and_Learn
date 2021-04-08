c02158205a2e641da50d4a9ae0a8a01c48416e85
WMXPY <wm@mengw.io>
2021-04-08 15:25:34 -0500

    add index view

---
+++ lib/entry/index.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LiveAndLearnIndexView extends StatefulWidget {
+  @override
+  _LiveAndLearnIndexViewState createState() => _LiveAndLearnIndexViewState();
+}
+
+class _LiveAndLearnIndexViewState extends State<LiveAndLearnIndexView> {
+  int _selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Live And Learn"),
+      ),
+      body: this._buildBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: "Learn",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: "History",
+          ),
+        ],
+        currentIndex: this._selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    switch (this._selectedIndex) {
+      case 0:
+        return Container();
+      case 1:
+        return Container();
+    }
+
+    return Container();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
