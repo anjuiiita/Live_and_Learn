c8347d2d14749c1b1a7eaf39eb7d5ab23236665f
WMXPY <wm@mengw.io>
2021-04-21 18:52:00 -0500

    rename live and learn

---
+++ live_and_learn/android/app/src/main/kotlin/com/example/live_and_learn/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.live_and_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // var methodChannel: MethodChannel? = null;
+
+    // override fun onCreate(savedInstanceState: Bundle?) {
+    //     super.onCreate(savedInstanceState);
+    //     methodChannel = MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, "app.sudo.land/live_and_learn/example");
+    // }
+
+    // override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+    //     return when (keyCode) {
+    //         KeyEvent.KEYCODE_VOLUME_DOWN -> {
+    //             methodChannel?.invokeMethod("PHYSICAL_BUTTON_UP", "VOLUME_DOWN");
+    //             true;
+    //         }
+    //         KeyEvent.KEYCODE_VOLUME_UP -> {
+    //             methodChannel?.invokeMethod("PHYSICAL_BUTTON_UP", "VOLUME_UP");
+    //             true;
+    //         }
+    //         else -> super.onKeyUp(keyCode, event);
+    //     }
+    // }
+}
